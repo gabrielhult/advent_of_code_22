22c5d916fdacaf2e689aa2389b028c2bce2f979c
Gabriel Hult <hultgabriel@live.se>
2022-11-29 15:40:40 +0100

    preparation commit for aoc22

---
+++ template.py
@@ -0,0 +1,21 @@
+def read_input():
+    with open("readme", "r") as f:
+        input_data = f.read().split("\n")
+        print(input_data)
+    return
+
+def part1():
+    return
+
+def part2():
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    read_input()
+    part1()
+    part2()
